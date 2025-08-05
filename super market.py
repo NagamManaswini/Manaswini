06b9c9d84e4a3f9197f568287b51b5c347b6e968
Manaswini <nagammanaswini@gmail.com>
2025-08-05 22:30:37 +0530

    Add files via upload

---
+++ super market.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+name=input("Enter the name: ")
+list='''
+rice  Rs 20 /kg
+dal   Rs 30/kg
+sugar Rs 40/kg
+salt  Rs 10/kg
+oil   Rs 50/litre
+'''
+print("Welcome to the Super Market")
+print(list)
+price =0
+pricelist=[]
+totalprice=0
+finalprice=0
+ilist=[]
+qlist=[]
+plist=[]
+
+items={'rice':20, 'dal':30, 'sugar':40, 'salt':10, 'oil':50}
+
